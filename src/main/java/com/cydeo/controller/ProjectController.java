f5634f4ac4e98dad49053a579dc7a05437dfca6f
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-05-21 13:02:46 -0500

    part-3 status code

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -34,23 +34,23 @@ public class ProjectController {
         return "/project/create";
 
     }
-//
-//    @PostMapping("/create")
-//    public String insertProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult bindingResult, Model model) {
-//
-//        if (bindingResult.hasErrors()) {
-//
-//            model.addAttribute("projects", projectService.findAll());
-//            model.addAttribute("managers", userService.findManagers());
-//
-//            return "/project/create";
-//
-//        }
-//
-//        projectService.save(project);
-//        return "redirect:/project/create";
-//
-//    }
+
+    @PostMapping("/create")
+    public String insertProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("projects", projectService.listAllProjects());
+            model.addAttribute("managers", userService.listAllByRole("manager"));
+
+            return "/project/create";
+
+        }
+
+        projectService.save(project);
+        return "redirect:/project/create";
+
+    }
 //
 //    @GetMapping("/delete/{projectcode}")
 //    public String deleteProject(@PathVariable("projectcode") String projectcode) {
