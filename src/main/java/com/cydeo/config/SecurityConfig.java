bbf79265ec7420036ac0216971cbe66135f31d72
civic <oleg16@gmail.com>
2023-06-09 16:47:26 -0500

    from office, correction: Security, added mike and ozzy to SecurityConfig

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig {
 
         userList.add(
                 new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
+        userList.add(
+                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
 
         return new InMemoryUserDetailsManager(userList);
     }
