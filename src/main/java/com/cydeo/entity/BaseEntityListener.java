77e838bd1711b8c642b73147450906bedf927eb6
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-06-14 03:28:22 -0500

    security-2

---
+++ src/main/java/com/cydeo/entity/BaseEntityListener.java
@@ -0,0 +1,46 @@
+package com.cydeo.entity;
+
+import com.cydeo.entity.common.UserPrincipal;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@Component
+public class BaseEntityListener extends AuditingEntityListener {
+
+    @PrePersist
+    public void onPrePersist(BaseEntity baseEntity){
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        baseEntity.insertDateTime = LocalDateTime.now();
+        baseEntity.lastUpdateDateTime = LocalDateTime.now();
+//        baseEntity.insertUserId = 1L;
+//        baseEntity.lastUpdateUserId = 1L;
+
+        if(authentication != null && !authentication.getName().equals("annonymousUser")){
+            Object principal = authentication.getPrincipal();
+            baseEntity.insertUserId = ((UserPrincipal) principal).getId();
+            baseEntity.lastUpdateUserId = ((UserPrincipal) principal).getId();
+        }
+    }
+
+    @PreUpdate
+    public void onPreUpdate(BaseEntity baseEntity){
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        baseEntity.lastUpdateDateTime = LocalDateTime.now();
+//        baseEntity.lastUpdateUserId = 1L;
+
+        if(authentication != null && !authentication.getName().equals("annonymousUser")){
+            Object principal = authentication.getPrincipal();
+            baseEntity.lastUpdateUserId = ((UserPrincipal) principal).getId();
+        }
+    }
+}
--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -61,4 +61,8 @@ public class UserPrincipal implements UserDetails {
     public boolean isEnabled() {
         return this.user.isEnabled();
     }
+
+    public Long getId(){
+        return this.user.getId();
+    }
 }
